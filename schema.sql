a5ce8835b1229ddf740363c46d6dfc47cf0ff7a6
karleighponnock <karleighponnock@gmail.com>
2020-07-15 20:35:33 -0400

    schema with 3 tables

---
+++ schema.sql
@@ -0,0 +1,39 @@
+--  Schema for the MySQL database. Add some seed values for testing 
+
+
+-- * **department**:
+DROP DATABASE IF EXISTS company_DB;
+CREATE database company_DB;
+
+USE company_DB;
+
+CREATE TABLE department_DB (
+    id INT(10) NOT NULL,
+    NAME VARCHAR(200)
+);
+
+SELECT * FROM department_DB;
+
+
+-- * **role**:
+CREATE TABLE role_DB (
+    id INT(10) NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,4) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+SELECT * FROM role_DB;
+
+-- * **employee**:
+CREATE TABLE employee_DB (
+    id INT(10) NOT NULL,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    role_id INT(10) NOT NULL,
+    manager_id INT(10) NULL
+);
+
+SELECT * FROM employee_DB;
+
---
+++ server.js
@@ -0,0 +1,10 @@
+// Requirements
+var express = require("express");
+var mysql = require("mysql");
+
+
+// * Add departments, roles, employees
+
+// * View departments, roles, employees
+
+// * Update employee roles
